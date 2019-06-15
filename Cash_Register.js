83698166dd7f592b7c85ecbb6d61501d93c35b3b
Amit Seetha <amitseetha@gmail.com>
2019-06-15 12:21:31 +0530

    Cash Register

--- Cash_Register.js
+++ Cash_Register.js
@@ -1,30 +1,23 @@
-Design a cash register drawer function checkCashRegister()that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument. 
+/*Design a cash register drawer function checkCashRegister()that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument. 
 cidis a 2D array listing available currency. 
 The checkCashRegister()function should always return an object with a statuskey and a changekey. 
 Return {status: "INSUFFICIENT_FUNDS", change: []}if cash-in-drawer is less than the change due, or if you cannot return the exact change. 
 Return {status: "CLOSED", change: [...]}with cash-in-drawer as the value for the key changeif it is equal to the change due. 
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the changekey. 
 Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code. 
-Currency Unit
-Amount
-Penny 
-$0.01 (PENNY) 
-Nickel 
-$0.05 (NICKEL) 
-Dime 
-$0.1 (DIME) 
-Quarter 
-$0.25 (QUARTER) 
-Dollar 
-$1 (DOLLAR) 
-Five Dollars 
-$5 (FIVE) 
-Ten Dollars 
-$10 (TEN) 
-Twenty Dollars 
-$20 (TWENTY) 
-One-hundred Dollars 
-$100 (ONE HUNDRED) 
+
+| CurrencyUnit        | Amount             |
+|---------------------|--------------------|
+| Penny               | $0.01 (PENNY)      |
+| Nickel              | $0.05 (NICKEL)     |
+| Dime                | $0.1 (DIME)        |
+| Quarter             | $0.25 (QUARTER)    |
+| Dollar              | $1 (DOLLAR)        |
+| Five Dollars        | $5 (FIVE)          |
+| Ten Dollars         | $10 (TEN)          |
+| Twenty Dollars      | $20 (TWENTY)       |
+| One Hundred Dollars | $100 (ONE HUNDRED) | 
+
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])should return an object. 
 Passed
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])should return {status: "OPEN", change: [["QUARTER", 0.5]]}. 
@@ -37,7 +30,7 @@ checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUAR
 Passed
 checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}. 
 
-/*
+
 1. Write a function to say true if you can't return a change. See Below.
 2. Declare variables status and change Array with keys same as cid and values =0.
 3. set up logic for change due and then logic for values for change object key values
